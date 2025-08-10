e0209ccbb96c3cf4466a4190522f3745d7ea5994
wetwetbeans <jackliamkwiecien@gmail.com>
2025-08-10 22:33:42 +0100

    Add missing JSON model types

---
+++ ScoreJsonModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+[Serializable]
+public record TimeSigJson(
+    int numerator,
+    int denominator,
+    int barIndex
+);
+
+[Serializable]
+public record KeySigJson(
+    int key,
+    int type,
+    int barIndex
+);
+
+[Serializable]
+public record TempoChangeJson(
+    double bpm,
+    int barIndex
+);
