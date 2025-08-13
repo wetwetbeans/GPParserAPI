ba60b91014f8ca972789efc7b45bc747f485076d
wetwetbeans <jackliamkwiecien@gmail.com>
2025-08-13 17:20:32 +0100

    Attempt 3

--- Program.cs
+++ Program.cs
@@ -6,150 +6,6 @@ using System.Text.Json;
 using System.Linq;
 using System.Reflection;
 
-// ---------------- JSON Records ----------------
-record ScoreJson(
-    string artist,
-    string title,
-    string album,
-    string subtitle,
-    string copyright,
-    string musicBy,
-    string wordsBy,
-    string transcriber,
-    string instructions,
-    string[] notices,
-    double tempo,
-    int ticksPerBeat,
-    int[] globalTuning,
-    string[] globalTuningText,
-    string globalTuningLetters,
-    TimeSigJson[] timeSignatures,
-    KeySigJson[] keySignatures,
-    TempoChangeJson[] tempoChanges,
-    MarkerJson[] markers,
-    RepeatJson[] repeats,
-    TrackJson[] tracks
-);
-
-record TrackJson(string name, int program, int channel, bool isPercussion, int capo, int transpose, int volume, int pan, StaffJson[] staves);
-record StaffJson(int[] tuning, string[] tuningText, string tuningLetters, BarJson[] bars);
-record BarJson(int index, bool repeatOpen, bool repeatClose, int repeatCount, int[] voltas, int barlineType, VoiceJson[] voices, TimeSigJson? timeSigOverride, MarkerJson? marker);
-record VoiceJson(BeatJson[] beats);
-record BeatJson(int start, int duration, int durationSymbol, int dots, (int num, int den) tuplet, bool isRest, int tremoloPicking, bool fadeIn, int arpeggio, int brushDirection, string whammyBarPoints, NoteJson[] notes);
-
-record NoteJson(
-    int @stringLow,
-    int @stringHigh,
-    int fret,
-    int pitchMidi,
-    bool isTieOrigin,
-    bool isTieDestination,
-    bool isGhost,
-    bool isDead,
-    bool isHarmonic,
-    int harmonicType,
-    double harmonicValue,
-    bool isPalmMute,
-    bool isLetRing,
-    bool isStaccato,
-    bool isHammerPullOrigin,
-    bool isHammerPullDestination,
-    bool isSlurOrigin,
-    bool isSlurDestination,
-    int slideInType,
-    int slideOutType,
-    int bendType,
-    BendPointJson[] bendPoints,
-    int vibratoType,
-    bool isTrill,
-    double trillValue,
-    int trillSpeed,
-    bool isTapped,
-    bool isSlapped,
-    bool isPopped,
-    int fingeringLeft,
-    int fingeringRight
-);
-
-record BendPointJson(double offset, double value);
-record TimeSigJson(int numerator, int denominator, int barIndex);
-record KeySigJson(int key, int type, int barIndex);
-record TempoChangeJson(double bpm, int barIndex);
-record MarkerJson(string text, int colorArgb, int barIndex);
-record RepeatJson(bool open, bool close, int count, int barIndex);
-
-// ---------------- Helper Methods ----------------
-static object? GetProp(object obj, string name) =>
-    obj?.GetType().GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(obj);
-
-static T GetProp<T>(object obj, string name, T defaultValue = default!)
-{
-    var val = GetProp(obj, name);
-    if (val is T t) return t;
-    try { return (T)Convert.ChangeType(val, typeof(T)); }
-    catch { return defaultValue; }
-}
-
-static int[] Tunings(Staff s) =>
-    s.StringTuning?.Tunings?.Select(v => (int)Math.Round(v)).ToArray() ?? Array.Empty<int>();
-
-static string[] TuningText(Staff s) =>
-    s.StringTuning?.Tunings?.Select(v => NoteNumberToString((int)Math.Round(v))).ToArray() ?? Array.Empty<string>();
-
-static string TuningLetters(Staff s) =>
-    string.Join(" ", TuningText(s));
-
-static string NoteNumberToString(int midiNote)
-{
-    string[] names = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-    return names[midiNote % 12] + (midiNote / 12);
-}
-
-static TimeSigJson[] CollectTimeSigs(Score s) =>
-    s.MasterBars.Select((mb, i) => new TimeSigJson(mb.TimeSignatureNumerator, mb.TimeSignatureDenominator, i)).ToArray();
-
-static KeySigJson[] CollectKeySigs(Score s) =>
-    s.MasterBars.Select((mb, i) => new KeySigJson((int)mb.KeySignature, (int)mb.KeySignatureType, i)).ToArray();
-
-static TempoChangeJson[] CollectTempos(Score s)
-{
-    var list = new List<TempoChangeJson>();
-    for (int i = 0; i < s.MasterBars.Count; i++)
-    {
-        var auto = GetProp(s.MasterBars[i], "TempoAutomation");
-        if (auto != null)
-        {
-            double bpm = GetProp(auto, "Value", 0.0);
-            if (bpm > 0) list.Add(new TempoChangeJson(bpm, i));
-        }
-    }
-    return list.ToArray();
-}
-
-static MarkerJson[] CollectMarkers(Score s)
-{
-    var list = new List<MarkerJson>();
-    for (int i = 0; i < s.MasterBars.Count; i++)
-    {
-        var section = s.MasterBars[i].Section;
-        if (section != null)
-            list.Add(new MarkerJson(section.Text ?? "", section.Color.ToArgb(), i));
-    }
-    return list.ToArray();
-}
-
-static RepeatJson[] CollectRepeats(Score s)
-{
-    var list = new List<RepeatJson>();
-    for (int i = 0; i < s.MasterBars.Count; i++)
-    {
-        var mb = s.MasterBars[i];
-        if (mb.RepeatClose || mb.RepeatOpen)
-            list.Add(new RepeatJson(mb.RepeatOpen, mb.RepeatClose, mb.RepeatCount, i));
-    }
-    return list.ToArray();
-}
-
 // ---------------- Top-level Program ----------------
 var builder = WebApplication.CreateBuilder(args);
 
@@ -329,3 +185,113 @@ app.MapPost("/parse", async (HttpRequest req) =>
 });
 
 app.Run();
+
+// ---------------- JSON Records ----------------
+record ScoreJson(
+    string artist,
+    string title,
+    string album,
+    string subtitle,
+    string copyright,
+    string musicBy,
+    string wordsBy,
+    string transcriber,
+    string instructions,
+    string[] notices,
+    double tempo,
+    int ticksPerBeat,
+    int[] globalTuning,
+    string[] globalTuningText,
+    string globalTuningLetters,
+    TimeSigJson[] timeSignatures,
+    KeySigJson[] keySignatures,
+    TempoChangeJson[] tempoChanges,
+    MarkerJson[] markers,
+    RepeatJson[] repeats,
+    TrackJson[] tracks
+);
+
+record TrackJson(string name, int program, int channel, bool isPercussion, int capo, int transpose, int volume, int pan, StaffJson[] staves);
+record StaffJson(int[] tuning, string[] tuningText, string tuningLetters, BarJson[] bars);
+record BarJson(int index, bool repeatOpen, bool repeatClose, int repeatCount, int[] voltas, int barlineType, VoiceJson[] voices, TimeSigJson? timeSigOverride, MarkerJson? marker);
+record VoiceJson(BeatJson[] beats);
+record BeatJson(int start, int duration, int durationSymbol, int dots, (int num, int den) tuplet, bool isRest, int tremoloPicking, bool fadeIn, int arpeggio, int brushDirection, string whammyBarPoints, NoteJson[] notes);
+record NoteJson(int @stringLow, int @stringHigh, int fret, int pitchMidi, bool isTieOrigin, bool isTieDestination, bool isGhost, bool isDead, bool isHarmonic, int harmonicType, double harmonicValue, bool isPalmMute, bool isLetRing, bool isStaccato, bool isHammerPullOrigin, bool isHammerPullDestination, bool isSlurOrigin, bool isSlurDestination, int slideInType, int slideOutType, int bendType, BendPointJson[] bendPoints, int vibratoType, bool isTrill, double trillValue, int trillSpeed, bool isTapped, bool isSlapped, bool isPopped, int fingeringLeft, int fingeringRight);
+record BendPointJson(double offset, double value);
+record TimeSigJson(int numerator, int denominator, int barIndex);
+record KeySigJson(int key, int type, int barIndex);
+record TempoChangeJson(double bpm, int barIndex);
+record MarkerJson(string text, int colorArgb, int barIndex);
+record RepeatJson(bool open, bool close, int count, int barIndex);
+
+// ---------------- Helper Methods ----------------
+static object? GetProp(object obj, string name) =>
+    obj?.GetType().GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(obj);
+
+static T GetProp<T>(object obj, string name, T defaultValue = default!)
+{
+    var val = GetProp(obj, name);
+    if (val is T t) return t;
+    try { return (T)Convert.ChangeType(val, typeof(T)); }
+    catch { return defaultValue; }
+}
+
+static int[] Tunings(Staff s) =>
+    s.StringTuning?.Tunings?.Select(v => (int)Math.Round(v)).ToArray() ?? Array.Empty<int>();
+
+static string[] TuningText(Staff s) =>
+    s.StringTuning?.Tunings?.Select(v => NoteNumberToString((int)Math.Round(v))).ToArray() ?? Array.Empty<string>();
+
+static string TuningLetters(Staff s) =>
+    string.Join(" ", TuningText(s));
+
+static string NoteNumberToString(int midiNote)
+{
+    string[] names = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+    return names[midiNote % 12] + (midiNote / 12);
+}
+
+static TimeSigJson[] CollectTimeSigs(Score s) =>
+    s.MasterBars.Select((mb, i) => new TimeSigJson(mb.TimeSignatureNumerator, mb.TimeSignatureDenominator, i)).ToArray();
+
+static KeySigJson[] CollectKeySigs(Score s) =>
+    s.MasterBars.Select((mb, i) => new KeySigJson((int)mb.KeySignature, (int)mb.KeySignatureType, i)).ToArray();
+
+static TempoChangeJson[] CollectTempos(Score s)
+{
+    var list = new List<TempoChangeJson>();
+    for (int i = 0; i < s.MasterBars.Count; i++)
+    {
+        var auto = GetProp(s.MasterBars[i], "TempoAutomation");
+        if (auto != null)
+        {
+            double bpm = GetProp(auto, "Value", 0.0);
+            if (bpm > 0) list.Add(new TempoChangeJson(bpm, i));
+        }
+    }
+    return list.ToArray();
+}
+
+static MarkerJson[] CollectMarkers(Score s)
+{
+    var list = new List<MarkerJson>();
+    for (int i = 0; i < s.MasterBars.Count; i++)
+    {
+        var section = s.MasterBars[i].Section;
+        if (section != null)
+            list.Add(new MarkerJson(section.Text ?? "", section.Color.ToArgb(), i));
+    }
+    return list.ToArray();
+}
+
+static RepeatJson[] CollectRepeats(Score s)
+{
+    var list = new List<RepeatJson>();
+    for (int i = 0; i < s.MasterBars.Count; i++)
+    {
+        var mb = s.MasterBars[i];
+        if (mb.RepeatClose || mb.RepeatOpen)
+            list.Add(new RepeatJson(mb.RepeatOpen, mb.RepeatClose, mb.RepeatCount, i));
+    }
+    return list.ToArray();
+}
